984419b7660723d36341b4fa4c031b7a1854b5eb
BVDPrasa-Arts <bethapudivenkatadurgaprasad@gmail.com>
2024-10-19 12:05:29 +0530

    first test

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,10 +1,8 @@
 function add(numbers) {
-  if (numbers.startsWith("//")) {
-      const [delimiter, nums] = numbers.slice(2).split('\n');
-      return nums.split(delimiter).reduce((sum, num) => sum + parseInt(num), 0);
-  }
   if (numbers === "") {
       return 0;
   }
-  return numbers.split(/[\n,]/).reduce((sum, num) => sum + parseInt(num), 0);
+  // Further implementation will go here.
 }
+
+module.exports = { add };
